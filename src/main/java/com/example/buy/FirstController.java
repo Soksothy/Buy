3a7232a4211fadd1c51333ee63015c071fa414f9
Sok Sothy <122692075+Soksothy@users.noreply.github.com>
2023-12-06 15:34:52 +0700

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/buy/FirstController.java
+++ src/main/java/com/example/buy/FirstController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -16,6 +17,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FirstController {
+
+
     @FXML
     private TextField usernameField;
     @FXML
@@ -28,7 +31,7 @@ public class FirstController {
     }
 
     public void switchToStaff(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("staff.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
