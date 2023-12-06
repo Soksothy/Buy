6bced16bfc6bcbfdeb3be40ea438641fe2872c5f
Sok Sothy <122692075+Soksothy@users.noreply.github.com>
2023-12-06 11:13:51 +0700

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/example/buy/FirstController.java
@@ -0,0 +1,59 @@
+package com.example.buy;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    private Map<String, String> userCredentials = new HashMap<>();
+    private Stage stage;
+    private Scene scene;
+    public FirstController() {
+        userCredentials.put("admin", "admin123");
+    }
+
+    public void switchToStaff(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("staff.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void login(ActionEvent event) throws IOException {
+        String enteredUsername = usernameField.getText();
+        String enteredPassword = passwordField.getText();
+
+        if (userCredentials.containsKey(enteredUsername) && userCredentials.get(enteredUsername).equals(enteredPassword)) {
+            Parent root = FXMLLoader.load(getClass().getResource("staff.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            showAlert("Login Failed", "Incorrect username or password.");
+        }
+    }
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+}
